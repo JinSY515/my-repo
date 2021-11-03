6cf9ce4869a5d09b0dca78fe89e39e9868bc71a3
unknown <73789740+JinSY515@users.noreply.github.com>
2021-11-03 23:13:17 +0900

    update

--- HW5/ex4_1.sql
+++ HW5/ex4_1.sql
@@ -1,6 +1,6 @@
 create table student (
     name varchar(20) NOT NULL,
-    id char(20) NOT NULL,
+    id int primary key auto_increment,
     year numeric(4,0) NOT NULL,
     major varchar(20) NOT NULL,
     phone char(11) NOT NULL,
@@ -8,5 +8,12 @@ create table student (
     tot_cred int NOT NULL DEFAULT 0,
     gpa numeric(2,1) NOT NULL,
     attending tinyint(1) NOT NULL DEFAULT 1,
-    PRIMARY KEY (id)
+    FOREIGN KEY (id) references student_id(id);
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+create table student_id(
+    id int primary key auto_increment,
+    year numeric(4,0) NOT NULL,
+    major numeric(2,0) NOT NULL,
+    individual numeric(4,0) NOT NULL
+)ENGINE = InnoDB DEFAULT CHARSET = utf8;
--- HW5/ex4_2.sql
+++ HW5/ex4_2.sql
@@ -1,54 +1,45 @@
 create table users (
-    id int auto_increment,
-    userid varchar(10) NOT NULL,
+    userid varchar(10) primary key NOT NULL,
     password varchar(20) NOT NULL,
     username varchar(15) NOT nULL,
     profile_img_link varchar(100) NOT NULL,
     profile_message varchar(100) NOT NULL,
     withdrawal tinyint(1) NOT NULL DEFAULT 0,
     join_date date NOT NULL,
-    primary key(id)
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 create table channels (
-    id int auto_increment,
     name varchar(20) NOT NULL,
-    userid varchar(10) NOT NULL,
+    userid varchar(10) primary key NOT NULL,
     link varchar(100) NOT NULL,
     capacity int NOT NULL,
     withdrawal tinyint(1) NOT NULL DEFAULT 0,
     join_date date NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(userid) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY(userid) REFERENCES users(userid) ON DELETE CASCADE
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 create table chats (
-    id int auto_increment,
     contents varchar(200) NOT NULL,
-    writer int NOT NULL,
-    channel int NOT NULL,
+    writer varchar(10) primary key NOT NULL,
+    channel varchar(100) NOT NULL,
     chat_date date NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(writer) REFERENCES users(id) on DELETE CASCADE,
-    FOREIGN KEY(channel) REFERENCES channels(id) ON DELETE CASCADE
+    FOREIGN KEY(writer) REFERENCES users(userid) on DELETE CASCADE,
+    FOREIGN KEY(channel) REFERENCES channels(link) ON DELETE CASCADE
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 create table follows (
-    id int auto_increment,
-    follower int NOT NULL,
-    followee int NOT NULL,
+    follower varchar(10) primary key NOT NULL,
+    followee varchar(10) NOT NULL,
     follow_date date NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(follower) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY(followee) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(follower) REFERENCES users(userid) ON DELETE CASCADE,
+    FOREIGN KEY(followee) REFERENCES users(userid) ON DELETE CASCADE,
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 create table blocks (
-    id int auto_increment,
-    blocks int NOT NULL,
-    blocked int NOT NULL,
+    blocks varchar(10) primary key NOT NULL,
+    blocked varchar(10) NOT NULL,
     block_date date NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(blocks) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY(blocked) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY(blocks) REFERENCES users(userid) ON DELETE CASCADE,
+    FOREIGN KEY(blocked) REFERENCES users(userid) ON DELETE CASCADE
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
